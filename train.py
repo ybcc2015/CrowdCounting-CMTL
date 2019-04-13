b29e148425c86b9bc8b8a865f589250a1bbd29bc
ybcc <271380178@qq.com>
2019-04-13 12:52:00 +0800

    add command line arguments

--- train.py
+++ train.py
@@ -21,7 +21,6 @@ def main(args):
     train_data_loader = DataLoader(train_path, train_gt_path, shuffle=True, gt_downsample=False)
     val_data_loader = DataLoader(val_path, val_gt_path, shuffle=False, gt_downsample=False)
     # 加载数据
-    print('Loading data, wait a moment...')
     train_X, train_Y_den, train_Y_class = train_data_loader.load_all()
     val_X, val_Y_den, val_Y_class = val_data_loader.load_all()
     class_weights = train_data_loader.get_class_weights()
@@ -30,10 +29,10 @@ def main(args):
     input_shape = (None, None, 1)
     model = CMTL(input_shape)
     adam = Adam(lr=0.00001)
-    loss = {'output_density': 'mse', 'output_class': 'categorical_crossentropy'}
-    loss_weights = {'output_density': 1.0, 'output_class': 0.0001}
+    loss = {'density': 'mse', 'cls': 'categorical_crossentropy'}
+    loss_weights = {'density': 1.0, 'cls': 0.0001}
     model.compile(optimizer=adam, loss=loss, loss_weights=loss_weights,
-                  metrics={'output_density': [MAE, MSE]})
+                  metrics={'density': [MAE, MSE]})
 
     # 定义callback
     checkpointer_best_train = ModelCheckpoint(
@@ -43,6 +42,15 @@ def main(args):
     callback_list = [checkpointer_best_train]
 
     # 训练
-    model.fit(train_X, {"output_density": train_Y_den, "output_class": train_Y_class},
-              validation_data=(val_X, {"output_density": val_Y_den, "output_class": val_Y_class}),
-              batch_size=1, epochs=cfg.EPOCHS, callbacks=callback_list)
+    model.fit(train_X,
+              {"density": train_Y_den, "cls": train_Y_class},
+              validation_data=(val_X, {"density": val_Y_den, "cls": val_Y_class}),
+              batch_size=1, epochs=cfg.EPOCHS, callbacks=callback_list,
+              class_weight={"cls": class_weights})
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="the dataset you want to train", choices=['A', 'B'])
+    args = parser.parse_args()
+    main(args)
